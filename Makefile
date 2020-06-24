2d1d033c797d9cdc3ff057098b784a72870680f1
takanotume24 <takanotume24@gmail.com>
2020-06-24 09:27:57 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+build:
+	mkdir -p bin
+	crystal build src/the_secretary_problem.cr -o bin/the_secretary_problem --release
---
+++ src/the_secretary_problem.cr
@@ -0,0 +1,11 @@
+# TODO: Write documentation for `TheSecretaryProblem`
+module TheSecretaryProblem
+  VERSION = "0.1.0"
+
+  def self.generate_applicants(n)
+    applicants = Array.new(n).shuffle
+  end
+  def self.main
+    
+  end
+end
